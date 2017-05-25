7022e5e1ec7b8244025e12bfce7a9d477a635258
James Awesome <james@wesome.nyc>
2017-05-25 17:37:26 -0400

    Get ticket viewer to vomit json from kanbantool

--- kanbantool_ticket_dash/ticket_viewer/views.py
+++ kanbantool_ticket_dash/ticket_viewer/views.py
@@ -1,8 +1,18 @@
-import os
+import os, json
 
 from . import ticket_viewer
-from flask import json, jsonify, current_app, render_template, request
+from flask import current_app, render_template, request
+import requests
+
+
+def fetch_tickets():
+    r = requests.get(
+            'https://grovo.kanbantool.com/api/v1/boards/316743/tasks.json',
+            params={'api_token': current_app.config['KANBANTOOL_API_KEY'], 'swimlane_id': '750005' }
+        )
+
+    return json.loads(r.text)
 
 @ticket_viewer.route('/')
 def index():
-    return render_template('ticket_viewer/index.html', kanbantool_api_key=current_app.config['KANBANTOOL_API_KEY'])
+    return render_template('ticket_viewer/index.html', tickets=json.dumps(fetch_tickets(), indent=4, sort_keys=True))
