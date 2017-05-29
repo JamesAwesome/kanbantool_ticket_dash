68d7e9452dbc539fba0f90c2f7a54dbe9658dad6
James Awesome <james@wesome.nyc>
2017-05-29 15:16:52 -0700

    use correct class FlaskForm not Form

--- kanbantool_ticket_dash/ticket_submitter/form.py
+++ kanbantool_ticket_dash/ticket_submitter/form.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateField
 from wtforms.validators import DataRequired, Optional, Regexp
 import arrow
 
-class TicketSubmitForm(Form):
+class TicketSubmitForm(FlaskForm):
       title        = StringField('Title', validators=[DataRequired()])
       submitted_by = StringField('Submitter', validators=[DataRequired()])
       due_date     = DateField('Due Date', format='%Y-%m-%d', validators=[Optional()], render_kw={"placeholder": "YYYY-MM-DD"})
