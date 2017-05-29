1d743134849ce73679e971c8d78c3e942dd1d92c
James Awesome <james@wesome.nyc>
2017-05-29 14:17:51 -0700

    Add better date validation

--- kanbantool_ticket_dash/ticket_submitter/form.py
+++ kanbantool_ticket_dash/ticket_submitter/form.py
@@ -1,11 +1,13 @@
 from flask_wtf import Form
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
-
+from wtforms import StringField, TextAreaField, DateField
+from wtforms.validators import DataRequired, Optional, Regexp
+import arrow
 
 class TicketSubmitForm(Form):
       title        = StringField('Title', validators=[DataRequired()])
       submitted_by = StringField('Submitter', validators=[DataRequired()])
-      due_date     = StringField('Due Date', validators=[DataRequired()])
+      due_date     = DateField('Due Date', format='%Y-%m-%d', validators=[Optional()], render_kw={"placeholder": "YYYY-MM-DD"})
       description  = TextAreaField('Description', validators=[DataRequired()])
 
+      def validate_due_date(form, field):
+          return arrow.get(field.data)
