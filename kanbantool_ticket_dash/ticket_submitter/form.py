b2aceef7c3838f9bf2072f09ab14882b2e82e0aa
James Awesome <james@wesome.nyc>
2017-05-29 16:04:28 -0700

    Add "(Optional)" to date field

--- kanbantool_ticket_dash/ticket_submitter/form.py
+++ kanbantool_ticket_dash/ticket_submitter/form.py
@@ -6,7 +6,7 @@ import arrow
 class TicketSubmitForm(FlaskForm):
       title        = StringField('Title', validators=[DataRequired()])
       submitted_by = StringField('Submitter', validators=[DataRequired()])
-      due_date     = DateField('Due Date', format='%Y-%m-%d', validators=[Optional()], render_kw={"placeholder": "YYYY-MM-DD"})
+      due_date     = DateField('Due Date', format='%Y-%m-%d', validators=[Optional()], render_kw={"placeholder": "YYYY-MM-DD (Optional)"})
       description  = TextAreaField('Description', validators=[DataRequired()])
 
       def validate_due_date(form, field):
