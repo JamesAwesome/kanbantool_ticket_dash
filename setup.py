e8df6220b77e14d24af580f214455df9fc008418
James Awesome <james@wesome.nyc>
2017-05-30 13:08:11 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='kanbantool_ticket_dash',
+    version='0.0.1',
+    packages=[
+        ''
+    ],
+    package_dir={'': 'kanbantool_ticket_dash'},
+    install_requires=[
+        'arrow >= 0.10, < 0.11',
+        'Flask >= 0.12.2, < 0.13',
+        'Flask-Script >= 2.0.5, < 2.1',
+        'Flask-WTF >= 0.14.2, < 0.15',
+        'requests >= 2.14.2, < 2.15'
+    ]
+)
