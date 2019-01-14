9d7932d75ab6374e03133e496074ee6b09259d1f
BobEckert <bob.eckert@slicelife.com>
2019-01-14 16:42:23 -0500

    Updated requirements

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     ],
     package_dir={'': 'kanbantool_ticket_dash'},
     install_requires=[
-        'arrow >= 0.10, < 0.11',
-        'Flask >= 0.12.2, < 0.13',
+        'arrow >=0.10,<0.14',
+        'Flask >=0.12.2,<1.1',
         'Flask-Script >= 2.0.5, < 2.1',
         'Flask-WTF >= 0.14.2, < 0.15',
-        'requests >= 2.14.2, < 2.15',
-        'gunicorn >= 19.7.1, < 19.8',
-        'slackclient >= 1.0.5, < 1.1',
+        'requests >=2.14.2,<2.22',
+        'gunicorn >=19.7.1,<19.10',
+        'slackclient >=1.0.5,<1.4',
     ]
 )
